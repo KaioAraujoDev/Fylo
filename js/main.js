3e4c94739496d674af7cb6c5b36b3850f13d81ef
KaioDev08 <kaioaraujo6190@outlook.com>
2022-08-29 20:04:53 -0300

    Adicionando lógica de exibição de retorno de espaço restante na memória

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const inputFile = document.querySelector('#arquivo');
 inputFile.addEventListener('change',(event)=>{
     const tamanho = (inputFile.files[0].size) / (1024*1024)
     exibirValor(tamanho.toFixed(2));
-    exibirRestante();
+    exibirRestante(tamanho.toFixed(2));
 });
 
 function exibirValor(valor){
@@ -18,6 +18,14 @@ function exibirValor(valor){
 }
 
 
-function exibirRestante(){
+function exibirRestante(size){
+    const divRestante = document.querySelector('#divRestante');
+    const outputRestante = divRestante.querySelector('#outputRestante');
+    outputRestante.innerText = 100 - size;
     
+    divRestante.classList.add('visible');
+    setTimeout(()=>{
+        divRestante.classList.remove('visible');
+    },3000);
+
 }
